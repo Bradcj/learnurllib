70811cedc8608b6486e791880ad08ac70d9eaf14
BradJ <j.chaiyaphop@gmail.com>
2021-09-07 02:03:43 +0700

    deleted:    readurl.py

--- readurl.py
+++
@@ -1,7 +0,0 @@
-import urllib.request, urllib.parse, urllib.error
-
-
-get_data = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-for line in get_data:
-    data = line.decode().strip()
-    print(data)
